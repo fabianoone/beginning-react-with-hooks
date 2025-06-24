a13f92d6076d721febb09eac4875c3c22f140dc0
Fabiano Oliveira <fabiano.one@gmail.com>
2025-06-24 11:43:40 -0300

    Embedding Expressions in JSX

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import Products from "./Products";
+
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
 
 function App() {
+    const user = {
+        firstName: 'Fabiano',
+        lastName: 'Oliveira',
+        imageUrl: 'https://avatars.githubusercontent.com/u/3976796?u=204d7abe10e367637229876cac02d3af9351ec78&v=4&size=64'
+    };
+
     return (
         <div>
-            <h1>Learn React Hooks!</h1>
+            <h1>Hello, {formatName(user)}</h1> <img src={user.imageUrl} alt={formatName(user)} />
+            <Products />
         </div>
     );
 }
