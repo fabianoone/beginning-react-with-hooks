23effd2fecff6ea1dff6571982b0d76ece207cb1
Fabiano Oliveira <fabiano.one@gmail.com>
2025-06-24 11:56:11 -0300

    Displaying a list

--- src/Products.js
+++ src/Products.js
@@ -1,7 +1,14 @@
 function Products() {
+    const products = ["Learning React", "Pro React", "Beginning React", "Up & Running React", "Learning JS"];
+
+    const listProducts = products.map((product) => <li key={product.toString()}>{product}</li>);
+
     return (
         <div>
             <h1>Products</h1>
+            {(products && products.length > 0) && (
+                <ul>{listProducts}</ul>
+            )}
         </div>
     );
 }
