6650eee938362ff30492b3e280463d17b34c0de6
Fabiano Oliveira <fabiano.one@gmail.com>
2025-07-02 17:32:56 -0300

    Showing a loader icon

--- src/Example.js
+++
@@ -1,9 +0,0 @@
-import styles from './example.module.css';
-
-export default function Example () {
-    return (
-        <>
-            <h1><span className={styles.bigblue} >This is</span> a Example component.</h1>
-        </>
-    );
-}
--- src/GitHub.js
+++ src/GitHub.js
@@ -1,19 +1,26 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
+import Spinner from 'react-bootstrap/Spinner';
 
 function GitHub() {
     const [data, setData] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         axios.get(`https://api.github.com/search/users?q=${searchTerm}`)
             .then(res => {
 //                console.log(res.data.items)
                     setData(res.data.items);
+                    setIsLoading(false);
             });
     }, []);
     return (
-        <div></div>
+        <div>
+            {isLoading &&
+                <Spinner animation="grow" variant="warning" />
+            }
+        </div>
     );
 }
 
