1e4f321f0d473137caf291a90dc18844181c2f56
Fabiano Oliveira <fabiano.one@gmail.com>
2025-07-02 18:23:34 -0300

    Add an input to GitHub results display page

--- src/GitHub.js
+++ src/GitHub.js
@@ -6,16 +6,26 @@ import Figure from 'react-bootstrap/Figure';
 function GitHub() {
     const [data, setData] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState( false);
 
     useEffect(() => {
-        axios.get(`https://api.github.com/search/users?q=fabiano`)
-            .then(res => {
-//                console.log(res.data.items)
-                    setData(res.data.items);
-                    setIsLoading(false);
-            });
+        getData();
     }, []);
+
+    const getData = () => {
+        axios.get(`https://api.github.com/search/users?q=${searchTerm}`)
+            .then((res) => {
+                setData(res.data.items)
+                setIsLoading(true);
+                getData();
+            })
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        setIsLoading(true);
+        getData();
+    };
 
     const listUsers = data.map((user) =>
         <Figure key={user.id} className="d-flex">
@@ -36,11 +46,40 @@ function GitHub() {
     );
 
     return (
-        <div className="d-flex flex-column">
-            {isLoading &&
-                <Spinner animation="grow" variant="warning" />
-            }
-            {listUsers}
+        <div>
+            <h3>GitHub Users Results</h3>
+            <form onSubmit={handleSubmit}>
+                <div className="form-row align-items-center">
+
+                  <div className="col-sm-6 my-1 p-0">
+                    <label className="sr-only" for="inlineFormInputGroupUsername">GitHub Username</label>
+                    <div className="input-group">
+                      <div className="input-group-prepend">
+                        <div className="input-group-text">@</div>
+                      </div>
+                      <input
+                          type="text"
+                          className="form-control"
+                          id="inlineFormInputGroupUsername"
+                          placeholder="GitHub Username"
+                          onChange={event => setSearchTerm(event.target.value)}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col-auto my-1 p-0">
+                    <button type="submit" className="btn btn-primary">Search</button>
+                  </div>
+                </div>
+              </form>
+
+
+            <div className="d-flex flex-column mt-5">
+                {isLoading &&
+                    <Spinner animation="grow" variant="warning" />
+                }
+                {listUsers}
+            </div>
         </div>
     );
 }
