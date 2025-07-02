f5a3fec98353731d6cdae89ed6f909422bfd0c6f
Fabiano Oliveira <fabiano.one@gmail.com>
2025-07-02 13:58:36 -0300

    Handling inputs with useState

--- src/UserForm.js
+++ src/UserForm.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Form, Button } from 'react-bootstrap';
 
 function UserForm() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
         <div>
             <Form>
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label column="sm">Email Address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control
+                        type="email"
+                        placeholder="Enter email"
+                        onChange={event => setEmail(event.target.value)}
+                    />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -15,12 +22,20 @@ function UserForm() {
 
                 <Form.Group controlId="formBasicPassword">
                     <Form.Label column="sm">Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control
+                        type="password"
+                        placeholder="Password"
+                        onChange={event =>setPassword(event.target.value)}
+                    />
                     <Button variant="primary" type="submit">
                         Submit
                     </Button>
                 </Form.Group>
             </Form>
+            <br />
+            Email entered: {email}
+            <br />
+            Password entered: {password}
         </div>
     );
 }
