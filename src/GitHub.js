3a7fa224b1264b7ac089be4d25698b58bd1181bd
Fabiano Oliveira <fabiano.one@gmail.com>
2025-07-02 17:47:01 -0300

    Implement a GitHub results display page

--- src/GitHub.js
+++ src/GitHub.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import Spinner from 'react-bootstrap/Spinner';
+import Figure from 'react-bootstrap/Figure';
 
 function GitHub() {
     const [data, setData] = useState([]);
@@ -8,18 +9,38 @@ function GitHub() {
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        axios.get(`https://api.github.com/search/users?q=${searchTerm}`)
+        axios.get(`https://api.github.com/search/users?q=fabiano`)
             .then(res => {
 //                console.log(res.data.items)
                     setData(res.data.items);
                     setIsLoading(false);
             });
     }, []);
+
+    const listUsers = data.map((user) =>
+        <Figure key={user.id} className="d-flex">
+            <a href={user.html_url}>
+                <Figure.Image
+                    width={64}
+                    height={64}
+                    className="mr-3"
+                    src={user.avatar_url}
+                    alt={user.login}
+                />
+            </a>
+            <Figure.Caption>
+                <h5>Login: {user.login}</h5>
+                <p>Id: {user.id}</p>
+              </Figure.Caption>
+        </Figure>
+    );
+
     return (
-        <div>
+        <div className="d-flex flex-column">
             {isLoading &&
                 <Spinner animation="grow" variant="warning" />
             }
+            {listUsers}
         </div>
     );
 }
