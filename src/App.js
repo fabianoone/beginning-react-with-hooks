6be0589128b305606630f65dc07f548f4d521bc0
Fabiano Oliveira <fabiano.one@gmail.com>
2025-07-12 15:35:37 -0300

    chp.8 - useReducer Hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
-import React, { useContext } from 'react';
-import { UserContext } from './index';
+import React, { useReducer} from "react";
+import { Button } from "react-bootstrap";
+
+const initialState = {
+    count: 0
+};
+
+function reducer(state, action) {
+    switch(action.type) {
+        case "increment":
+            return { count: state.count + 1}
+        case "decrement":
+            return { count: state.count -1}
+        case "reset":
+            return initialState
+        default:
+            return initialState
+    }
+}
 
 function App() {
-    const value = useContext(UserContext);
-  return (
-    <div>
-        Received, {value}
-    </div>
-  );
+    const [state, dispatch] = useReducer(reducer, initialState);
+    return (
+        <>
+            <div>
+                Count: {state.count}
+                <br />
+                <br />
+                <Button onClick={() => dispatch({type: "increment"})}>
+                    increment
+                </Button>
+                <br />
+                <br />
+                <Button variant="secondary" onClick={() => dispatch({type: 'decrement'})}>
+                    Decrement
+                </Button>
+                <br />
+                <br />
+                <Button variant="danger" onClick={() => dispatch({type: 'reset'})}>Reset</Button>
+            </div>
+        </>
+    );
 }
+
+
 
 export default App;
